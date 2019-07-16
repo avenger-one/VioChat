5f84b9420f25e9e2a87db33679c1a1b10939945f
Violanes <theshershnyov@yandex.ru>
2019-07-16 20:26:35 +0300

    fixed some weird crashes

--- src/space/deniska/VioChat.java
+++ src/space/deniska/VioChat.java
@@ -12,7 +12,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Set;
 
-public class Main extends JavaPlugin
+public class VioChat extends JavaPlugin
 {
 
     private static String AdminSymbol = "@";
